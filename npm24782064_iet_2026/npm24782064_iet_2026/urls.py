8dbe43305288cc49035bcde11da2b46c40c3b363
syifaamalia12 <amaliasyifa657@gmail.com>
2026-03-15 00:16:38 +0700

    Lab02

--- npm24782064_iet_2026/npm24782064_iet_2026/urls.py
+++ npm24782064_iet_2026/npm24782064_iet_2026/urls.py
@@ -1,11 +1,9 @@
 from django.contrib import admin
-from django.urls import path
-from django.http import HttpResponse
-
-def welcome(request):
-    return HttpResponse("Selamat Datang")
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('welcome/', welcome),
+    path('', include('main_app.urls')),
+    path('about/', include('about.urls')),
+    path('contacts/', include('contacts.urls')),
 ]
